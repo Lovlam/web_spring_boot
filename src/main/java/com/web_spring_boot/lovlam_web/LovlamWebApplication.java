0623627a28a24b2b160a77a93c6774792271649e
BaooLam <duongbaolam2214@gmail.com>
2024-01-05 00:04:18 +0700

    add all file project

---
+++ src/main/java/com/web_spring_boot/lovlam_web/LovlamWebApplication.java
@@ -0,0 +1,13 @@
+package com.web_spring_boot.lovlam_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovlamWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovlamWebApplication.class, args);
+	}
+
+}
